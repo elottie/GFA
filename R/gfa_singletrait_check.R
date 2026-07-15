b9c5c573cac5666181ac43d2c472fa9de5918e8a
Charlotte Edwards <elottie@gl-login2.arc-ts.umich.edu>
2026-07-15 11:55:38 -0400

    added print statements for debugging singletrait_check

--- R/gfa_singletrait_check.R
+++ R/gfa_singletrait_check.R
@@ -31,15 +31,20 @@ gfa_singletrait_check <- function(fit, check_thresh = 0.9, params){
          if(x == 2) return(TRUE)
          return(FALSE)})
     }
+    print(paste('fixed_ix from flashier:',fixed_ix))
+
 
     Dn <- norm_cols(D)$A
     col_max <- apply(abs(Dn), 2, max)
 
     single_trait_index <- which(col_max > check_thresh & !fixed_ix)
+    print(paste('first time we see single_trait_index:',single_trait_index))
     single_traits <- sapply(single_trait_index, function(i){
       which.max(abs(Dn[,i]))
     })
+    print(paste('single_traits:',single_traits))
     single_trait_index <- single_trait_index[!single_traits %in% checked_trait]
+    print(paste('second time we see single_trait_index:',single_trait_index))
     if(length(single_trait_index) == 0){
       if(upd_this_round){
         checked_trait <- c()
